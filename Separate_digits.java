536171a2d830876eda168e14ca2739121d6f9d12
Vasanthakumar Saravanan <vasanthakumar.saravanan.01@gmail.com>
2026-07-12 23:47:22 +0530

    day 6 added Separate_digits

---
+++ Separate_digits.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Separate_digits {
+
+    public int[] separateDigits(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int x : nums) {
+            List<Integer> tmp = new ArrayList<>();
+            while (x > 0) {
+                tmp.add(x % 10);
+                x /= 10;
+            }
+            for (int i = tmp.size() - 1; i >= 0; i--) {
+                res.add(tmp.get(i));
+            }
+        }
+
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}
